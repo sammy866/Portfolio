4565ae3d521b534e43e942181d99791eefee2579
sammy866 <samosammy7@gmail.com>
2023-04-09 14:08:11 +0100

    Portfolio section added. Scroll up button added

--- js/main.js
+++ js/main.js
@@ -71,6 +71,20 @@ tabs.forEach(tab => {
     })
 })
 
+/* PORTFOLIO SWIPER */
+let swiper = new Swiper('.portfolio_container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    },
+
+});
 /* CONTACT US FORM TO GOOGLE SHEETS */
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwbrD5eHpcUNrMx3Xjr7hHdXAeuhvVAd7o2736HdVbtRa8dImoNOq2svqJqX8seG3gKFg/exec'
@@ -85,3 +99,37 @@ form.addEventListener('submit', e => {
         })
         .catch(error => console.error('Error!', error.message))
 })
+
+/* SCROLL SECTION ACTIVE LINK */
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/* BACKGROUND HEADER */
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/* SHOW SCROLL UP */
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
